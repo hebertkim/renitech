26619129690dc3ceb7004b7d7cf28affa86ce172
hebertkim <hebert.kim@hotmail.com>
2026-01-24 04:58:27 -0300

    Refatoramneto para correção de bug

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,9 +4,12 @@
 # Importar todos os models para registro no SQLAlchemy
 # =========================
 
+from .company import Company
+from .store import Store
 from .user import User
 from .category import ProductCategory
 from .product import Product
 from .product_image import ProductImage
 from .stock import StockMovement, StockMovementType
+from .customer import Customer
 from .order import Order, OrderItem
